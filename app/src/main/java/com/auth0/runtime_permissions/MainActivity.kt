29a15aa4613499b5a61ac19bbc91bedb35bb55e5
Luciano Balmaceda <balmacedaluciano@gmail.com>
2021-08-17 13:25:32 +0200

    add runtime permission logic

--- app/src/main/java/com/auth0/runtime_permissions/MainActivity.kt
+++ app/src/main/java/com/auth0/runtime_permissions/MainActivity.kt
@@ -1,11 +1,89 @@
 package com.auth0.runtime_permissions
 
+import android.Manifest
+import android.content.res.ColorStateList
+import android.graphics.Color
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.content.PermissionChecker
+import com.auth0.runtime_permissions.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val permissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { granted -> updatePermissionsUI(granted) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_welcome)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding.buttonLogin.setOnClickListener { launchLogin() }
+        binding.buttonPermissionRetry.setOnClickListener { requestPermissions() }
+
+        setContentView(binding.root)
+        updatePermissionsUI()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        requestPermissions()
+    }
+
+    private fun updatePermissionsUI(granted: Boolean = checkPermissionsAreGranted()) {
+        if (granted) {
+            binding.imageMap.imageTintList =
+                ColorStateList.valueOf(Color.parseColor("#4CAF50"))
+            binding.buttonPermissionRetry.isEnabled = false
+        } else {
+            binding.imageMap.imageTintList =
+                ColorStateList.valueOf(Color.parseColor("#F44336"))
+            binding.buttonPermissionRetry.isEnabled = true
+        }
+    }
+
+    private fun checkPermissionsAreGranted(): Boolean {
+        return ContextCompat.checkSelfPermission(
+            this,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        ) == PermissionChecker.PERMISSION_GRANTED
+    }
+
+    private fun requestPermissions() {
+        val alreadyGranted = checkPermissionsAreGranted()
+        if (alreadyGranted) {
+            // permission was previously granted
+            updatePermissionsUI(true)
+            return
+        }
+
+        val shouldExplain =
+            shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION)
+        if (!shouldExplain) {
+            // show Android permission request dialog
+            permissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+            return
+        }
+
+        // show a friendly UI explaining why the permission is required by the app
+        AlertDialog.Builder(this)
+            .setTitle("Location required")
+            .setMessage("This activity requires access to your location. \n\nGrant it to display the image in GREEN or reject it to keep it RED.")
+            .setPositiveButton("Continue") { dialog, _ ->
+                // accepting will prompt the user with the Android permission request dialog
+                permissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+                dialog.dismiss()
+            }
+            .setNegativeButton("No way") { dialog, _ ->
+                dialog.dismiss()
+            }
+            .show()
+    }
+
+    private fun launchLogin() {
+        TODO("Not yet implemented")
     }
 }
--- app/src/main/java/com/auth0/runtime_permissions/WelcomeActivity.kt
+++ app/src/main/java/com/auth0/runtime_permissions/WelcomeActivity.kt
@@ -1,11 +1,25 @@
 package com.auth0.runtime_permissions
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.auth0.runtime_permissions.databinding.ActivityWelcomeBinding
 
 class WelcomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_welcome)
+
+        val binding = ActivityWelcomeBinding.inflate(layoutInflater)
+        binding.buttonLogin.setOnClickListener {
+            Toast.makeText(this, "Not implemented", Toast.LENGTH_SHORT).show()
+        }
+        binding.buttonGuest.setOnClickListener {
+            val mainActivity = Intent(this, MainActivity::class.java)
+            startActivity(mainActivity)
+            finish()
+        }
+
+        setContentView(binding.root)
     }
 }
